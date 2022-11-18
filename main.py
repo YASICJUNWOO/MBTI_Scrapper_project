097cfdff5c97e578db21afb64d247abc9dafe97c
midterm <midterm@naver.com>
2022-11-19 00:34:54 +0900

    html 페이지 생성

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
-from mm import extract_mbti_overview
-from flask import Flask
+from extract_mbit import extract_mbti_overview
+from flask import Flask,render_template,request,send_file
 
 app = Flask("mbti-tracker")
 
+db = {}
+
 @app.route("/")
 def home():
-    return "<h1>assfdsafsdaf!</h1>"
+    return render_template("home.html")
+
+@app.route("/result")
+def result():
+    mbti = request.args.get("mbti")
+    if mbti in db:
+        mbti_data = db[mbti]
+    else:
+        mbti_data = extract_mbti_overview(mbti)
+        db[mbti] = mbti_data
+    print(mbti_data)
+    return render_template("result.html", mbti_data=mbti_data)
 
 
 #data = extract_mbti_overview(mbti)
